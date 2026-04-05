1245aa055141751c04adad6c2df5d69293626fe3
Sterling1111 <sterlingijeppson@gmail.com>
2026-04-05 01:11:55 -0700

    did some things and stuff

--- Calculus/Chapter20/Prelude.v
+++ Calculus/Chapter20/Prelude.v
@@ -1,3 +1,4 @@
-From Lib Require Export Imports Sets Limit Continuity Derivative Integral Notations Reals_util Inverse Functions Interval Tactics Trigonometry.
+From Lib Require Export Imports Sets Limit Continuity Derivative Integral Exponential
+         Notations Reals_util Inverse Functions Interval Tactics Trigonometry Taylor.
 Export LimitNotations IntervalNotations SetNotations DerivativeNotations FunctionNotations IntegralNotations.
 Open Scope R_scope.
--- test.v
+++
@@ -1,3 +0,0 @@
-From Calculus.Chapter23 Require Import Prelude.
-Lemma test : exists S, ∑ 0 ∞ (fun n => INR n) = S.
-Admitted.
