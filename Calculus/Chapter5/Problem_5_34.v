865fdd841d435a37c6e20e32437eb1431994d860
Sterling1111 <sterlingijeppson@gmail.com>
2026-04-10 06:36:44 -0700

    refactor: enhance proofs for lemmas 5_34 and 5_37 in Chapter 5

--- Calculus/Chapter5/Problem_5_34.v
+++ Calculus/Chapter5/Problem_5_34.v
@@ -1,5 +1,8 @@
 From Calculus.Chapter5 Require Import Prelude.
 
-Lemma lemma_5_34 : ∀ (f : R -> R) L,
-  ⟦ lim 0⁺ ⟧ (fun x => f (1 / x)) = L <-> ⟦ lim ∞ ⟧ f = L.
-Proof. Admitted.
+Lemma lemma_5_34 : ∀ f L,
+  ⟦ lim 0⁺ ⟧ (λ x, f (1 / x)) = L <-> ⟦ lim ∞ ⟧ f = L.
+Proof.
+  intros f L. split.
+  - intros H1 N H2.
+Admitted.
--- Calculus/Chapter5/Problem_5_37.v
+++ Calculus/Chapter5/Problem_5_37.v
@@ -1,6 +1,19 @@
 From Calculus.Chapter5 Require Import Prelude.
 
-Lemma lemma_5_37_a : ⟦ lim 3 ⟧ (fun x => 1 / (x - 3)^2) = ∞.
+Lemma lemma_5_37_a : ⟦ lim 3 ⟧ (λ x, 1 / (x - 3)^2) = ∞.
 Proof.
-  intros M. 
-Admitted.
+  unfold limit_to_pinf.
+  intros N.
+  exists ( 1 / √(Rmax 1 N) ).
+  assert (H1 : √(Rmax 1 N) > 0) by (apply sqrt_lt_R0; solve_R).
+  split; [apply Rdiv_pos_pos; lra |].
+  intros x [H2 H3].
+  assert (H4 : (x - 3) * (x - 3) < 1 / √Rmax 1 N * (1 / √Rmax 1 N)) by solve_R.
+  replace ((1 / √(Rmax 1 N)) * (1 / √(Rmax 1 N))) with (1 / (√ (Rmax 1 N) * √(Rmax 1 N))) in H4 by solve_R.
+  rewrite sqrt_sqrt in H4; [ | solve_R].
+  assert (H5 : / (1 / Rmax 1 N) < / ((x - 3) * (x - 3))).
+  { apply Rinv_lt_contravar; try lra. apply Rmult_lt_0_compat; solve_R. }
+  replace (/ (1 / Rmax 1 N)) with (Rmax 1 N) in H5 by solve_R.
+  replace (1 / (x - 3) ^ 2) with (/ ((x - 3) * (x - 3))) by solve_R.
+  solve_R.
+Qed.
