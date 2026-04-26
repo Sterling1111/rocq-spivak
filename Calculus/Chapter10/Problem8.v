b4d8721103def72352f99cfb774b9d011868a28f
Sterling1111 <sterlingijeppson@gmail.com>
2026-04-26 11:51:50 -0700

    refactor: reorganize variable definitions and update lemmas for area and circumference calculations

--- Calculus/Chapter10/Problem8.v
+++ Calculus/Chapter10/Problem8.v
@@ -2,47 +2,82 @@ From Calculus.Chapter11 Require Import Prelude.
 
 Section Section_10_8.
 
-Definition Area (r : ℝ) := π * r^2.
+Definition Area (r : ℝ) := π*r^2.
 Definition Circumference (r : ℝ) := 2*π*r.
 
-Variable r : ℝ -> ℝ.
-Hypothesis H1 : forall t, r t > 0.
+Variable r1 r2 r1' r2' : ℝ -> ℝ.
 
-Definition A_S (t : ℝ) := Area (r t).
-Definition C_S (t : ℝ) := Circumference (r t).
+Hypothesis H1 : ∀ t, r1 t > 0.
+Hypothesis H2 : ∀ t, r2 t > 0.
+Hypothesis H3 : ∀ t, r1 t < r2 t.
+Hypothesis H4 : ⟦ der ⟧ r1 = r1'.
+Hypothesis H5 : ⟦ der ⟧ r2 = r2'.
 
-Definition A_L (t : ℝ) := A_S t + 9*π.
+Definition A1 (t : ℝ) := Area (r1 t).
+Definition A2 (t : ℝ) := Area (r2 t).
+Definition A1' := (fun t => 2 * π * r1 t * r1' t).
+Definition A2' := (fun t => 2 * π * r2 t * r2' t).
 
-Variable r' : ℝ -> ℝ.
-Hypothesis H2 : ⟦ der ⟧ r = r'.
-
-Lemma A_S_derivative : ⟦ der ⟧ A_S = (fun t => 2 * π * r t * r' t).
+Lemma A1_derivative : ⟦ der ⟧ A1 = A1'.
 Proof.
-  unfold A_S, Area. auto_diff.
+  unfold A1, Area, A1'. auto_diff.
 Qed.
 
-Lemma A_L_derivative : ⟦ der ⟧ A_L = (fun t => 2 * π * r t * r' t).
+Lemma A2_derivative : ⟦ der ⟧ A2 = A2'.
 Proof.
-  unfold A_L, A_S, Area. auto_diff.
+  unfold A2, Area, A2'. auto_diff.
 Qed.
 
-Lemma C_S_derivative : ⟦ der ⟧ C_S = (fun t => 2 * π * r' t).
+Hypothesis H6 : ∀ t, A2 t - A1 t = 9*π.
+Hypothesis H7 : ∀ t, A2' t = 10*π.
+
+Lemma rate_relation : ∀ t, 10*π - 2*π * r1 t * r1' t = 0.
 Proof.
-  unfold C_S, Circumference. auto_diff.
+  intros t.
+  
+  assert (H8 : A2 = (fun t => A1 t + 9 * π)).
+  { extensionality x. pose proof (H6 x). lra. }
+  
+  assert (H9 : ⟦ der ⟧ A2 = A1').
+  { rewrite H8. unfold A1, A1', Area. auto_diff. }
+  
+  pose proof derivative_at_unique A2 A2' A1' t (A2_derivative t) (H9 t) as H10.
+
+  rewrite H7 in H10.
+  unfold A1' in H10.
+  lra.
 Qed.
 
-Variable t0 : ℝ.
+Definition C1 (t : ℝ) := Circumference (r1 t).
+Definition C1' := (fun t => 2 * π * r1' t).
 
-Hypothesis H3 : 2 * π * r t0 * r' t0 = 10 * π.
-
-Hypothesis H4 : A_S t0 = 16 * π.
-
-Lemma lemma_10_8 : 2 * π * r' t0 = 5 * π / 2.
+Lemma C1_derivative : ⟦ der ⟧ C1 = C1'.
 Proof.
-  unfold A_S, Area in H4.
-  pose proof (H1 t0) as H5.
-  pose proof π_pos as H6.
-  nra.
+  unfold C1, Circumference, C1'. auto_diff.
+Qed.
+
+Variable t : ℝ.
+
+Hypothesis H8 : A1 t = 16 * π.
+
+Lemma lemma_10_8 : C1' t = 5 * π / 2.
+Proof.
+  unfold C1', A1, Area in *.
+  pose proof (rate_relation t) as H9.
+  pose proof (H1 t) as H10.
+  pose proof π_pos as H11.
+
+  assert (H12 : r1 t = 4).
+  {
+    unfold A1, Area in H8.
+    apply Rmult_eq_compat_r with (r := / (π)) in H8.
+    field_simplify in H8; nra.
+  }
+
+  assert (H13 : r1' t = 5 / 4).
+  { rewrite H12 in H9. nra. }
+
+  rewrite H13. field.
 Qed.
 
 End Section_10_8.
