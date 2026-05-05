59b7e3c4e31fe9c1e3443004735b1758f042f449
Sterling1111 <sterlingijeppson@gmail.com>
2026-05-04 21:36:19 -0700

    feat: formalize proofs for derivative exercises 18 and 19 in Calculus Chapter 10

--- Calculus/Chapter10/Problem18.v
+++ Calculus/Chapter10/Problem18.v
@@ -5,7 +5,16 @@ Lemma lemma_10_18_a : ∀ f g f' g',
   ⟦ der ⟧ f = f' ->
   ⟦ der ⟧ g = g' ->
   g' = (λ x, 2 * f x * f' x).
-Proof. Abort.
+Proof.
+  intros f g f' g' H1 H2 H3.
+  assert (H4: g = f ⋅ f).
+  { extensionality x. rewrite H1. lra. }
+  rewrite H4 in H3.
+  assert (H5 := derivative_mult f f f' f' H2 H2).
+  apply derivative_unique with (f1' := (f' ⋅ f + f ⋅ f')%function) in H3; auto.
+  rewrite <- H3.
+  extensionality x. lra.
+Qed.
 
 Lemma lemma_10_18_b : ∀ f g f' f'' g',
   (∀ x, g x = (f' x)^2) ->
@@ -13,7 +22,10 @@ Lemma lemma_10_18_b : ∀ f g f' f'' g',
   ⟦ der ⟧ f' = f'' ->
   ⟦ der ⟧ g = g' ->
   g' = (λ x, 2 * f' x * f'' x).
-Proof. Abort.
+Proof.
+  intros f g f' f'' g' H1 H2 H3 H4.
+  exact (lemma_10_18_a f' g f'' g' H1 H3 H4).
+Qed.
 
 Lemma lemma_10_18_c : ∀ f,
   (∀ x, f x > 0) ->
--- Calculus/Chapter10/Problem19.v
+++ Calculus/Chapter10/Problem19.v
@@ -1,14 +1,38 @@
 From Calculus.Chapter10 Require Import Prelude.
 
-Definition Schwarzian_derivative (f : ℝ -> ℝ) (x : ℝ) : ℝ :=
-  ⟦ Der x ⟧ (⟦ Der ⟧ (⟦ Der ⟧ f)) / ⟦ Der x ⟧ f - 3 / 2 * (⟦ Der x ⟧ (⟦ Der ⟧ f) / ⟦ Der x ⟧ f)^2.
+Definition Schwarzian_derivative (f : R -> R) (x : R) : R :=
+  ⟦ Der ^ 3 x ⟧ f / ⟦ Der x ⟧ f - 3 / 2 * (⟦ Der ^ 2 x ⟧ f / ⟦ Der x ⟧ f)^2.
+
+Notation "'D'" := Schwarzian_derivative.
 
 Lemma lemma_10_19_a : ∀ f g x,
-  Schwarzian_derivative (f ∘ g) x = Schwarzian_derivative f (g x) * (⟦ Der x ⟧ g)^2 + Schwarzian_derivative g x.
-Proof. Abort.
+  nth_differentiable_at 3 g x ->
+  nth_differentiable_at 3 f (g x) ->
+  ⟦ Der x ⟧ g <> 0 ->
+  ⟦ Der (g x) ⟧ f <> 0 ->
+  D (f ∘ g) x = D f (g x) * (⟦ Der x ⟧ g)^2 + D g x.
+Proof.
+  intros f g x H1 H2 H3 H4.
+
+  set (f' := ⟦ Der ⟧ f).
+  set (f'' := ⟦ Der ⟧ f').
+  set (f''' := ⟦ Der ⟧ f'').
+  set (g' := ⟦ Der ⟧ g).
+  set (g'' := ⟦ Der ⟧ g').
+  set (g''' := ⟦ Der ⟧ g'').
+
+  assert (H5 : ⟦ Der x ⟧ (f ∘ g) = f' (g x) * g' x).
+  { apply derive_at_comp; apply nth_differentiable_at_imp_differentiable_at with (n := 3%nat); auto; lia. }
+
+  assert (H6 : ⟦ Der ^ 2 x ⟧ (f ∘ g) = f'' (g x) * g' x ^2 + f' (g x) * g'' x).
+  {
+    
+  }
+
+Admitted.
 
 Lemma lemma_10_19_b : ∀ a b c d,
   a * d - b * c ≠ 0 ->
   ∀ x, c * x + d ≠ 0 ->
-  Schwarzian_derivative (λ x, (a * x + b) / (c * x + d)) x = 0.
+  D (λ x, (a * x + b) / (c * x + d)) x = 0.
 Proof. Abort.
