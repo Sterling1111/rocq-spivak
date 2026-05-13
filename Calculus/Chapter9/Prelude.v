db81dff945d0a8772a241b51c5d57bcd1aec02fa
Sterling1111 <sterlingijeppson@gmail.com>
2026-05-12 22:10:26 -0700

    feat: update Prelude and implement lemma for differentiability in Problem 28

--- Calculus/Chapter9/Prelude.v
+++ Calculus/Chapter9/Prelude.v
@@ -1,3 +1,3 @@
-From Lib Require Export Imports Limit Reals_util Notations Rational Sets Derivative Trigonometry Tactics Interval Functions Exponential.
+From Lib Require Export Imports Limit Reals_util Notations Rational Sets Continuity Derivative Trigonometry Tactics Interval Functions Exponential.
 Export LimitNotations SetNotations IntervalNotations FunctionNotations DerivativeNotations.
 Open Scope R_scope.
--- Calculus/Chapter9/Problem28.v
+++ Calculus/Chapter9/Problem28.v
@@ -1,16 +1,23 @@
 From Calculus.Chapter9 Require Import Prelude.
 
-Lemma lemma_9_28_a : forall f f' f'' x,
-  f = (fun x => (| x |)^3) -> 
-  ⟦ der ⟧ f = f' -> ⟦ der ⟧ f' = f'' -> 
-  f' x = 3 * x * | x | /\ f'' x = 6 * | x |.
+Lemma lemma_9_28 : forall f g,
+  (forall x, f x = x * g x) -> 
+  continuous_at g 0 -> 
+  differentiable_at f 0 /\ ⟦ der 0 ⟧ f = g.
 Proof.
-Abort.
-
-Lemma lemma_9_28_b : forall f f' f'' x,
-  (forall x, x >= 0 -> f x = x^4) ->
-  (forall x, x <= 0 -> f x = -x^4) ->
-  ⟦ der ⟧ f = f' -> ⟦ der ⟧ f' = f'' -> 
-  f' x = 4 * (| x |) ^ 3 /\ f'' x = 12 * x * | x |.
-Proof.
-Abort.
+  intros f g H1 H2.
+  assert (⟦ der 0 ⟧ f = g) as H3.
+  {
+    intros ε H3.
+    destruct (H2 ε H3) as [δ [H4 H5]].
+    exists δ; split; auto.
+    intros x H6.
+    simp_zero.
+    specialize (H5 x H6).
+    replace ((f x - f 0) / x - g 0) with (g x - g 0).
+    2 : { do 2 rewrite H1. solve_R. }
+    exact H5.
+  }
+  split; auto.
+  apply derivative_at_imp_differentiable_at with (f' := g); auto.
+Qed.
