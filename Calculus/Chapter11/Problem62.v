89e88b191160a99251f47b0dec6350058b8c4d5c
Sterling1111 <sterlingijeppson@gmail.com>
2026-05-17 23:30:58 -0700

    feat: refine lemma 11.62 proof by restructuring cases for differentiability

--- Calculus/Chapter11/Problem62.v
+++ Calculus/Chapter11/Problem62.v
@@ -6,7 +6,16 @@ Lemma lemma_11_62 : forall f a,
   differentiable_at f a.
 Proof.
   intros f a H1 H2.
-  assert (f a = 0 \/ f a <> 0) as [H3 | H3] by lra.
+  destruct (Rtotal_order (f a ) 0) as [H3 | [H3 | H3]].
+  - apply differentiable_at_eq with (f1 := fun x => -1 * |f x|).
+    { 
+      pose proof continuous_at_locally_neg f a H1 H3 as [δ [H4 H5]].
+      exists δ; split; auto.
+      intros x H6.
+      specialize (H5 x H6).
+      solve_R.
+    }
+    apply differentiable_at_mult_const_l; auto.
   - assert (minimum_point (λ x : ℝ, |(f x)|) (a - 1, a + 1) a) as H4 by (split; solve_R).
     pose proof derivative_at_minimum_point_zero (λ x, |(f x)|) (a - 1) (a + 1) a H4 H2 as H5.
     unfold derivative_at, differentiable_at in *.
@@ -20,5 +29,9 @@ Proof.
     rewrite H3, Rabs_R0, Rminus_0_r, Rminus_0_r, Rabs_div in *.
     rewrite Rabs_Rabsolu in H8.
     exact H8.
-  -
-Abort.
+  - apply differentiable_at_eq with (f1 := λ x, |f x|); auto.
+    pose proof continuous_at_locally_pos f a H1 H3 as [δ [H4 H5]].
+    exists δ. split; auto.
+    intros x H6. specialize (H5 x H6).
+    solve_R.
+Qed.
