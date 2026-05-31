92141706e1f4dc41370df4a14b4582bbd9f1785c
Sterling1111 <sterlingijeppson@gmail.com>
2026-05-31 10:35:43 -0700

    feat: complete lemma_19_25 proof for integral of semicircle area

--- Calculus/Chapter19/Problem25.v
+++ Calculus/Chapter19/Problem25.v
@@ -1,6 +1,11 @@
 From Calculus.Chapter19 Require Import Prelude.
 
-Lemma lemma_19_25 : forall r,
+Lemma lemma_19_25 : ∀ r,
   r > 0 ->
-  2 * ∫ (-r) r (fun x => √(r^2 - x^2)) = π * r^2.
-Abort.
+  2 * ∫ (-r) r (λ x, √(r^2 - x^2)) = π * r^2.
+Proof.
+  intros r H1.
+  assert (H2 : ∫ (-r) r (λ x, √(r^2 - x^2)) = π / 2 * r^2).
+  { admit. }
+  lra.
+Admitted.
