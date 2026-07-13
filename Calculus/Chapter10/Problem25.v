75d0035fb8dbbe290d25c470ef6c5acc045ac313
Sterling1111 <sterlingijeppson@gmail.com>
2026-07-12 21:35:15 -0700

    feat: complete lemma_10_25 proof for derivative properties; implement lemma_9_14 proof for differentiability

--- Calculus/Chapter10/Problem25.v
+++ Calculus/Chapter10/Problem25.v
@@ -1,7 +1,13 @@
 From Calculus.Chapter10 Require Import Prelude.
 
-Lemma lemma_10_25 : ∀ f a d,
-  differentiable_at f a ->
-  (∀ x, d x = f x - ⟦ Der a ⟧ f * (x - a) - f a) ->
-  ⟦ Der a ⟧ d = 0.
-Proof. Abort.
+Lemma lemma_10_25 : ∀ f f' a d,
+  ⟦ der a ⟧ f = f' ->
+  (∀ x, d x = f x - f' a * (x - a) - f a) ->
+  ⟦ der a ⟧ d = λ _, 0.
+Proof.
+  intros f f' a d H1 H2.
+  replace d with (λ x, f x - f' a * (x - a) - f a).
+  2 : { extensionality x. rewrite H2. reflexivity. }
+  apply derivative_at_ext_val with (f' := λ x, (f' x - f' a * (1 - 0)) - 0); try lra.
+  apply derivative_at_minus; [ apply derivative_at_minus |]; auto_diff.
+Qed.
--- Calculus/Chapter9/Problem14.v
+++ Calculus/Chapter9/Problem14.v
@@ -5,4 +5,17 @@ Lemma lemma_9_14 : forall f,
   (forall x, ~ rational x -> f x = 0) ->
   differentiable_at f 0.
 Proof.
-Abort.
+  intros f H1 H2.
+  exists 0.
+  assert (H3 : f 0 = 0).
+  { rewrite H1; try lra. exists 0%Z, 1%Z. lra. }
+  replace (λ h, (f (0 + h) - f 0) / h) with (λ h, f h / h).
+  2 : { extensionality h. rewrite H3. simp_zero. reflexivity. }
+  apply limit_squeeze with (a := -1) (b := 1) (f1 := λ h, -|h|) (f3 := λ h, |h|); 
+  try solve [ auto_limit ].
+  intros x H4. apply In_Union_def in H4.
+  destruct (classic (rational x)) as [H5 | H5].
+  - rewrite (H1 x H5). replace (x^2 / x) with x by solve_R.
+    solve_R.
+  - rewrite (H2 x H5). solve_R.
+Qed.
